b0fc0505da6a06bb4ffba2d5add9d8e915a776b6
wallywallywally <willsonhanzhekai@gmail.com>
2024-01-22 23:15:03 +0800

    Complete Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
+        // intro
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatbotName = "Wallybot";
+
+        System.out.println("Hello! I'm " + chatbotName + "!");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye, hope to see you again soon!");
     }
 }
